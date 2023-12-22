67bb49cab0b01a9e502d9c2196c47ee10a85a4ea
AndrejPitomec <148689748+AndrejPitomec@users.noreply.github.com>
2023-12-22 09:41:31 +0100

    Add files via upload

---
+++ Project16/src/Proekt/Proekt.java
@@ -0,0 +1,10 @@
+package Proekt;
+
+public class Proekt {
+
+	public static void main(String[] args) {
+		System.out.println("Test programa za da se uplodne na git hub");
+        //AndrejPitomec
+	}
+
+}
